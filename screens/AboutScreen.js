cdbce0d63f8f08be752d363dc56bccbe00724128
Hanna Koivisto <hekoivisto@gmail.com>
2019-12-17 12:01:22 +0200

    About-osio tyylittelyjä vaille valmis

--- screens/AboutScreen.js
+++ screens/AboutScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, Linking, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, Linking, TouchableOpacity, ScrollView } from 'react-native';
 import Icon from "react-native-vector-icons/MaterialIcons";
 
 export default class AboutScreen extends Component {
@@ -12,7 +12,7 @@ export default class AboutScreen extends Component {
     }
     render() {
         return (
-            <View style={styles.container}>
+            <ScrollView style={styles.container}>
                 <Text style={styles.title}>
                     Tietoja sovelluksesta
                 </Text>
@@ -28,48 +28,101 @@ export default class AboutScreen extends Component {
                         https://www.myhelsinki.fi/</Text>
                 </Text>
                 <Text style={styles.body}>
-                    Sovelluksen taustakuva: "Helsinki Christmas Market" {"\n"}
+                    Sovelluksen taustakuva: {"\n"}
+                    "Helsinki Christmas Market" {"\n"}
                     Valokuvaaja: Jussi Hellsten {"\n"}
-                    Valokuvan lähde: http://materialbank.myhelsinki.fi/media/915</Text>
-                <Text style={styles.body}>Tapahtumat- ja aktiviteetit-osioissa näkyvien valokuvien tiedot:</Text>
+                    Valokuvan lähde: {"\n"}
+                    http://materialbank.myhelsinki.fi/media/915</Text>
+                <Text style={styles.body}>Tapahtumat- ja aktiviteetit-osioissa näkyvien valokuvien tiedot löytyvät pudotusvalikosta. {"\n"}
+                    Sovelluksessa käytety kuvat ovat Helsingin materiaalipankista materialbank.myhelsinki.fi, jonka kuvat ovat vapaasti käytettävissä ei-kaupalliseen käyttöön.</Text>
+
                 <TouchableOpacity style={styles.row} onPress={() => this.toggleExpand()}>
-                    {/* <Text style={[styles.title, styles.font]}>{this.props.title}</Text> */}
-                    <Icon name={this.state.expanded ? 'keyboard-arrow-up' : 'keyboard-arrow-down'} size={30} />
+                    <Text style={styles.title}>Kuvatiedot</Text>
+                    <Icon name={this.state.expanded ? 'keyboard-arrow-up' : 'keyboard-arrow-down'} size={35} color={'white'} />
                 </TouchableOpacity>
-                <View style={styles.parentHr} />
+                <View style={styles.accordion} />
                 {
                     this.state.expanded &&
-                    <View style={styles.child}>
-                        {/* <Text>{this.props.data}ENTÄ JOS KIRJOITAN TÄNNE</Text> */}
-                        <Text>
-                            http://materialbank.myhelsinki.fi/media/2883 (Tuomas Uusheimo) {"\n"}
-                            http://materialbank.myhelsinki.fi/media/536 (Jussi Hellsten) {"\n"}
-                            http://materialbank.myhelsinki.fi/media/895 (Jussi Hellsten) {"\n"}
-                            http://materialbank.myhelsinki.fi/media/1342 (Dorit Salutskij) {"\n"}
-                            http://materialbank.myhelsinki.fi/media/2372 (Yiping Feng and Ling Ouyang) {"\n"}
-                            http://materialbank.myhelsinki.fi/media/2374 (Yiping Feng and Ling Ouyang) {"\n"}
-                            http://materialbank.myhelsinki.fi/media/914 (Jussi Hellsten) {"\n"}
-                            http://materialbank.myhelsinki.fi/media/1149 (Eetu Ahanen) {"\n"}
-                            http://materialbank.myhelsinki.fi/media/3602 (Jussi Hellsten) {"\n"}
-                            http://materialbank.myhelsinki.fi/media/1080 (Riku Pihlanto) {"\n"}
-                            http://materialbank.myhelsinki.fi/media/3284 (Jussi Hellsten) {"\n"}
-                            http://materialbank.myhelsinki.fi/media/2663 (Tuomas Uusheimo) {"\n"}
-                            http://materialbank.myhelsinki.fi/media/2645 (Tuomas Uusheimo) {"\n"}
-                            http://materialbank.myhelsinki.fi/media/2222 (Yinping Feng and Ling Ouyang) {"\n"}
-                            http://materialbank.myhelsinki.fi/media/1086 (Robert Lindström) {"\n"}
-                            http://materialbank.myhelsinki.fi/media/3407 (Vesa Laitinen) {"\n"}
-                            http://materialbank.myhelsinki.fi/media/2767 (Samuli Vienola) {"\n"}
+                    <View>
+                        <Text style={styles.body}>
+                            Kuva: "Oodi - central library's main entrance", {"\n"}
+                            Kuvaaja: Tuomas Uusheimo, {"\n"}
+                            Lähde: http://materialbank.myhelsinki.fi/media/2883 {"\n"}
+                            {"\n"}
+                            Kuva: "Teurastamo Abattoir", {"\n"}
+                            Kuvaaja: Jussi Hellsten, {"\n"}
+                            Lähde: http://materialbank.myhelsinki.fi/media/536 {"\n"}
+                            {"\n"}
+                            Kuva: "Kallio Block Party"
+                            Kuvaaja: Jussi Hellsten
+                            Lähde: http://materialbank.myhelsinki.fi/media/895 {"\n"}
+                            {"\n"}
+                            Kuva: "Allas Sea Pool, Skywheel in the background", {"\n"}
+                            Kuvaaja: Dorit Salutskij, {"\n"}
+                            Lähde: http://materialbank.myhelsinki.fi/media/1342 {"\n"}
+                            {"\n"}
+                            Kuva: "Enjoying coffee and doughnut at the Hakaniemi Market Square", {"\n"}
+                            Kuvaajat: Yiping Feng and Ling Ouyang, {"\n"}
+                            Lähde: http://materialbank.myhelsinki.fi/media/2372 {"\n"}
+                            {"\n"}
+                            Kuva: "Hakaniemi Market Square", {"\n"}
+                            Kuvaajat: Yiping Feng and Ling Ouyang, {"\n"}
+                            Lähde: http://materialbank.myhelsinki.fi/media/2374 {"\n"}
+                            {"\n"}
+                            Kuva: "Food yard at the Christmas Market in Senate Square", {"\n"}
+                            Kuvaaja: Jussi Hellsten, {"\n"}
+                            Lähde: http://materialbank.myhelsinki.fi/media/914 {"\n"}
+                            {"\n"}
+                            Kuva: "Baltic Herring Market is held by the sea", {"\n"}
+                            Kuvaaja: Eetu Ahanen, {"\n"}
+                            http://materialbank.myhelsinki.fi/media/1149 {"\n"}
+                            {"\n"}
+                            Kuva: "Christmas lights in Helsinki (3)", {"\n"}
+                            Kuvaaja: Jussi Hellsten, {"\n"}
+                            Lähde: http://materialbank.myhelsinki.fi/media/3602 {"\n"}
+                            {"\n"}
+                            Kuva: "Tennis Palace", {"\n"}
+                            Kuvaaja: Riku Pihlanto, {"\n"}
+                            Lähde: http://materialbank.myhelsinki.fi/media/1080 {"\n"}
+                            {"\n"}
+                            Kuva: "Helsinki Christmas Market", {"\n"}
+                            Kuvaaja: Jussi Hellsten, {"\n"}
+                            Lähde: http://materialbank.myhelsinki.fi/media/3284 {"\n"}
+                            {"\n"}
+                            Kuva: "Central library Oodi - third floor reading room", {"\n"}
+                            Kuvaaja: Tuomas Uusheimo, {"\n"}
+                            http://materialbank.myhelsinki.fi/media/2663 {"\n"}
+                            {"\n"}
+                            Kuva: "OODI - hide-away spots", {"\n"}
+                            Kuvaaja: Tuomas Uusheimo, {"\n"}
+                            Lähde: http://materialbank.myhelsinki.fi/media/2645 {"\n"}
+                            {"\n"}
+                            Kuva: "Amos Rex lobby", {"\n"}
+                            Kuvaajat: Yinping Feng and Ling Ouyang, {"\n"}
+                            Lähde: http://materialbank.myhelsinki.fi/media/2222 {"\n"}
+                            {"\n"}
+                            Kuva: "Old Market Hall", {"\n"}
+                            Kuvaaja: Robert Lindström, {"\n"}
+                            Lähde: http://materialbank.myhelsinki.fi/media/1086 {"\n"}
+                            {"\n"}
+                            Kuva: "Summer Day in Töölönlahti event", {"\n"}
+                            Kuvaaja: Vesa Laitinen, {"\n"}
+                            Lähde: http://materialbank.myhelsinki.fi/media/3407 {"\n"}
+                            {"\n"}
+                            Kuva: "Saving New Year’s Eve celebration memories", {"\n"}
+                            Kuvaaja: Samuli Vienola, {"\n"}
+                            Lähde: http://materialbank.myhelsinki.fi/media/2767 {"\n"}
                         </Text>
                     </View>
                 }
-            </View>
+            </ScrollView>
         );
     }
     toggleExpand = () => {
         this.setState({ expanded: !this.state.expanded })
     }
 }
-// https://medium.com/@KPS250/creating-an-accordion-in-react-native-f313748b7b46
+
 const styles = StyleSheet.create({
     container: {
         flex: 1,
@@ -92,19 +145,18 @@ const styles = StyleSheet.create({
     },
     row: {
         flexDirection: 'row',
-        // justifyContent: 'space-between',
+        justifyContent: 'space-between',
         height: 56,
-        paddingLeft: 25,
-        paddingRight: 18,
+        paddingTop: 15,
+        paddingLeft: 15,
+        paddingRight: 15,
         alignItems: 'center',
-        backgroundColor: 'white',
+        backgroundColor: '#00000000',
     },
-    parentHr: {
+    accordion: {
         height: 1,
-        color: 'green',
-        width: '100%'
-    },
-    child: {
-        padding: 16,
+        color: 'white',
+        width: '100%',
+        fontSize: 16,
     }
 });
